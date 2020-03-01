9781a16c1342bcb7b595ca7d0b81326a8dc21c8f
Constantin Kurz <constantin.kurz@aol.com>
2020-03-01 16:56:41 +0100

    static files debut

--- TipApp/settings.py
+++ TipApp/settings.py
@@ -117,7 +117,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_TMP = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 # os.makedirs(STATIC_TMP, exist_ok=True)
